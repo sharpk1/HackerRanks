62186752e8a167799368532a5eccc75b2876f944
Kush Shah <kushdshah@yahoo.com>
2020-12-20 13:35:28 -0600

    adding highest average algorithm

---
+++ HighestAverage.js
@@ -0,0 +1,40 @@
+function highestAverage(scores){
+    // create a key value dictionary
+    let gradeBook = {};
+    let len = scores.length;
+    let studentName;
+    let studentScore;
+    let existingGrade;
+    let max = 0;
+    
+    for (let i = 0; i < len; i++){
+        studentName = scores[i][0];
+        
+        if (studentName in gradeBook){
+            existingGrade = parseInt(gradeBook[studentName]);
+            studentScore = parseInt(scores[i][1]);
+            let average = (existingGrade + studentScore) / 2;
+            gradeBook[studentName] = average;
+            
+        }else{
+            gradeBook[studentName] = scores[i][1];
+        }
+    }
+
+    Object.keys(gradeBook).forEach(function (grade) {
+        if (gradeBook[grade] > max){
+            max = gradeBook[grade];
+        }
+    });
+
+    return max;
+    
+}
+
+
+
+let testScores = [['Kush', '87'],['Charles', '100'],['Jay', '50'],['Charles', '22']];
+
+
+// return the highest average
+highestAverage(testScores);
