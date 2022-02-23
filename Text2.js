575fd64ccf0715873f97e26f166db886328f54b0
Kush Shah <kushdshah@yahoo.com>
2022-02-22 18:49:02 -0600

    refactoring

--- Text2.js
+++ Text2.js
@@ -2,189 +2,159 @@
 // Create a more consistent state
 
 const textEditor = (input) => {
-    let output = [];
-    let cursorPosition = 0;
-    let clipboard = [];
-    let documents = []; // [documentName, cursor position, document data]
-    let lastKnownState = {
-        content: -1,
+    let currentDocument = {
+        title: 'document0',
+        output: [],
         selection: false,
         selectionStart: -1,
         selectionEnd: -1,
-        actions: [],
-        previousState: []
     }
+    let cursorPosition = 0;
+    let clipboard = [];
+    let documents = []; // [documentName, cursor position, document data]
     let isSelect = false;
     let endPosition = -1;
-    let undoQueue = [];
     let redoQueue = [];
     let previousState = [];
-    let currentDocument = {};
-
-    for (let i = 0; i < input.length; i++){
-        input[i].unshift(`${i}`);
-    }
+    
 
     for (inputLine of input){
         
 
-        const [index, operation, contentOrStartPosition, endingPosition] = inputLine;
+        const [operation, contentOrStartPosition, endingPosition] = inputLine;
+
         switch (operation) {
             case "APPEND":
-
-                lastKnownState.content = output;
-                lastKnownState.selection = isSelect;
+                currentDocument.selection = isSelect;
                 if (isSelect){
-                    if (lastKnownState.selectionEnd != -1){
-                        endPosition = lastKnownState.selectionEnd;
+                    if (currentDocument.selectionEnd != -1){
+                        endPosition = currentDocument.selectionEnd;
                     }
-                    let tempOutput = output.join('');
+                    let tempOutput = currentDocument.output.join('');
                     tempOutput = tempOutput.split('');
                     tempOutput.splice(cursorPosition, endPosition - cursorPosition, contentOrStartPosition);
                     cursorPosition = parseInt(cursorPosition) + contentOrStartPosition.length;
-                    output = tempOutput;
+                    currentDocument.output = tempOutput;
                     isSelect = false;
-                    lastKnownState.selectionStart = cursorPosition;
-                    lastKnownState.selectionEnd = endPosition;
+                    currentDocument.selectionStart = cursorPosition;
+                    currentDocument.selectionEnd = endPosition;
                     endPosition = -1;
-                }else if (cursorPosition !== output.join('').length - 1){
-                    
-                    let tempOutput = output.join('');
+                }else if (cursorPosition !== currentDocument.output.join('').length - 1){
+                    let tempOutput = currentDocument.output.join('');
                     tempOutput = tempOutput.split('');
                     tempOutput.splice(cursorPosition, 0, contentOrStartPosition);
                     cursorPosition = parseInt(cursorPosition) + contentOrStartPosition.length;
-                    output = tempOutput;
-
-
+                    currentDocument.output = tempOutput;
                 }else {
-                    output.push(contentOrStartPosition);
+                    currentDocument.output.push(contentOrStartPosition);
                     cursorPosition += contentOrStartPosition.length;
-                    
                 }
-                lastKnownState.actions.push(operation);
                 break;
             case "MOVE":
                 cursorPosition = contentOrStartPosition;
                 endPosition = -1;
-                lastKnownState.actions.push(operation);
-
                 break;
             case "DELETE":
-                previousState.push(output)
-                lastKnownState.selection = isSelect;
+                previousState.push(currentDocument.output)
+                currentDocument.selection = isSelect;
                 if (isSelect){
-                    
-                    let tempOutput = output.join('');
+                    let tempOutput = currentDocument.output.join('');
                     tempOutput = tempOutput.split('');
                     tempOutput.splice(cursorPosition, endPosition - cursorPosition);
-                    output = tempOutput;
+                    currentDocument.output = tempOutput;
                     isSelect = false;
-                    lastKnownState.selectionStart = cursorPosition;
-                    lastKnownState.selectionEnd = endPosition;
+                    currentDocument.selectionStart = cursorPosition;
+                    currentDocument.selectionEnd = endPosition;
                     endPosition = -1;
-                    
                 }else {
-                    let tempOutput = output.join('');
+                    let tempOutput = currentDocument.output.join('');
                     tempOutput = tempOutput.split('');
                     tempOutput.splice(cursorPosition, 1);
-                    output = tempOutput;
+                    currentDocument.output = tempOutput;
                 }
-                
-                lastKnownState.actions.push(operation);
                 break;
             case "SELECT":
                 isSelect = true;
                 cursorPosition = contentOrStartPosition;
                 endPosition = endingPosition;
-                lastKnownState.actions.push(operation);
-
                 break;
             case "CUT":
                 // do what DELETE and COPY does
-                lastKnownState.content = output;
-                lastKnownState.selection = isSelect;
+                currentDocument.selection = isSelect;
                 if (isSelect){
-                    let tempOutput = output.join('');
+                    let tempOutput = currentDocument.output.join('');
                     tempOutput = tempOutput.split('');
                     clipboard.push(tempOutput.slice(cursorPosition, endPosition).join(''));
                     tempOutput.splice(cursorPosition, endPosition - cursorPosition);
-                    output = tempOutput;
+                    currentDocument.output = tempOutput;
                     isSelect = false;
-                    lastKnownState.selectionStart = cursorPosition;
-                    lastKnownState.selectionEnd = endPosition;
+                    currentDocument.selectionStart = cursorPosition;
+                    currentDocument.selectionEnd = endPosition;
                     endPosition = -1;
                 }
-                lastKnownState.actions.push(operation);
                 break;
             case "PASTE":
-                lastKnownState.content = output;
-                lastKnownState.selection = isSelect;
+                currentDocument.selection = isSelect;
                 let tempClipboard = clipboard.join('');
-                let tempOutput = output.join('');
+                let tempOutput = currentDocument.output.join('');
 
                 tempOutput = tempOutput.split('');
                 tempOutput.splice(cursorPosition, 0, tempClipboard);
                 cursorPosition = parseInt(cursorPosition) + tempClipboard.length;
-                output = tempOutput;
-                lastKnownState.actions.push(operation);
+                currentDocument.output = tempOutput;
                 break;
             case "UNDO":
-                // UNDO - take previous state, apply it to output, and take current state and place it in redoQueue
-                // REDO - take output and place it in undoQueue, take last from redoQueue, apply it to output and 
-                if (lastKnownState.actions.slice(-1)[0] != "MOVE" || lastKnownState.actions.slice(-1)[0] != "SELECT"){
-                    if (previousState.length == 0){
-                        redoQueue.push(output)
-                        output = [""];
-                        isSelect = lastKnownState.selection;
-                    } else {
-                        
-                        let temp = previousState.pop();
-                        redoQueue.push(output);
-                        output = temp;
-                        isSelect = lastKnownState.selection;                        
-                    }
-                }
+                // UNDO - take previous state, apply it to currentDocument.output, and take current state and place it in redoQueue
+                // REDO - take currentDocument.output and place it in undoQueue, take last from redoQueue, apply it to currentDocument.output and
+                if (previousState.length == 0){
+                    redoQueue.push(currentDocument.output)
+                    currentDocument.output = [""];
+                    isSelect = currentDocument.selection;
+                } else {
+                    let temp = previousState.pop();
+                    redoQueue.push(currentDocument.output);
+                    currentDocument.output = temp;
+                    isSelect = currentDocument.selection;                        
+                } 
                 break;
             case "REDO":
                 // Needs to be after UNDO
                 // Restore previous state including CURSOR POSITION and SELECTION
                 if (redoQueue.length != 0){
                     let tempRedo = redoQueue.pop();
-                    output = tempRedo;
+                    currentDocument.output = tempRedo;
                     break;
                 }
             case "CREATE":
                 // Create object with document title, document data, and state
                 let found = documents.find(o => o.title === contentOrStartPosition);
                 if (found){
-                    return "";
+                    console.log("");
+                } else {
+                    documents.push(currentDocument);
+                    documents.push({
+                        title: contentOrStartPosition,
+                        output: [],
+                        selection: false,
+                        selectionStart: -1,
+                        selectionEnd: -1,
+                    });
                 }
-                documents.push({
-                    "title": contentOrStartPosition,
-                    "content": [],
-                    "state": {
-                        "cursorPosition": 0,
-                        "endSelection": -1
-                    },
-                    "modificationHistory": []
-                });
+
                 break;
             case "SWITCH":
-                currentDocument = documents.find(o => o.title === contentOrStartPosition);
-                if (!currentDocument){
-                    return "";
+                try {
+                    currentDocument = documents.find(o => o.title === contentOrStartPosition);
+                    // console.log('document is now ', currentDocument.title);
+                } catch (error) {
+                    console.log(error);
                 }
                 break;
-
-
         }
-
-        
-
-
     }
-    // console.log(output.join(''))
-    return output.join('');
+    // console.log(currentDocument);
+    // console.log(currentDocument.output.join(''));
+    return currentDocument.output.join('');
 }
 
 const query1 = [
@@ -264,6 +234,20 @@ const query9 = [
     
 ]
 
+const query10 = [
+    ["CREATE", "document1"],
+    ["CREATE", "document2"],
+    ["CREATE", "document1"],
+    ["SWITCH", "document1"],
+    ["APPEND", "Hello, world!"],
+    ["SELECT", "7", "12"],
+    ["CUT"],
+    ["SWITCH", "document2"],
+    ["PASTE"],
+    ["SWITCH", "document1"],
+    ["DELETE"]
+]
+
 // UNDO - take previous state, apply it to output, and take current state and place it in redoQueue
 // REDO - take output and place it in undoQueue, take last from redoQueue, apply it to output and 
 console.log(textEditor(query1) == "Hey, you");
@@ -275,3 +259,4 @@ console.log(textEditor(query6) == "Hello, you!");
 console.log(textEditor(query7) == "Hello, !");
 console.log(textEditor(query8) == "og");
 console.log(textEditor(query9) == "");
+console.log(textEditor(query10) == "Hello,!");
